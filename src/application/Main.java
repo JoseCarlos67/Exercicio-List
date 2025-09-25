eca464fcebef9f5ed30803cafe6f9c6b79ca51e2
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-25 12:02:36 -0300

    Classe employee feita (nao testada) e base do main feita

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,19 @@
 package application;
 
+import java.io.ByteArrayInputStream;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    String fakeInputString = "3\n333\nMaria Brown\n4000.00\n536\nAlex Grey\n3000.00\n772\nBob Green\n5000.00\n536\n10.0";
+    ByteArrayInputStream fakeInput = new ByteArrayInputStream(fakeInputString.getBytes());
+    System.setIn(fakeInput);
+
+    Scanner scanner = new Scanner(System.in);
+
 
   }
 }
---
+++ src/application/entitties/Employee.java
@@ -0,0 +1,38 @@
+package application.entitties;
+
+public class Employee {
+  private Integer id;
+  private String name;
+  private Double salary;
+
+  public Employee(Integer id, String name, Double salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getSalary() {
+    return salary;
+  }
+
+  public void salaryIncrease(double amount) {
+    salary += (salary / 10) * 100;
+  }
+
+  @Override
+  public String toString() {
+    return id + ", " + name + ", " + String.format("%.2f", salary);
+  }
+}
